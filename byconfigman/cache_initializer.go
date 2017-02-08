456fdef8c93e08b646d5d6491ddb028a3b855030
Reza Mohammadi <reza@cafebazaar.ir>
2017-02-08 18:53:42 +0330

    Add empty byconfigman structure

---
+++ byconfigman/cache_initializer.go
@@ -0,0 +1,14 @@
+package byconfigman
+
+import (
+	"github.com/cafebazaar/configman"
+	"github.com/cafebazaar/hafezieh"
+)
+
+// NewCache selects a cache engine nased on the name value in config, and
+// contructs it using the config values, and returns the prepared
+// hafezieh.Cache instance
+func NewCache(config configman.ConfigManager) hafezieh.Cache {
+	// TODO
+	return nil
+}
