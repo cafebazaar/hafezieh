ce7fae001bf4436a022d13b32302aed5c0353ecc
Reza Mohammadi <reza@cafebazaar.ir>
2017-02-13 14:42:24 +0330

    Fix LRU Cleanup

--- inmemory/cleanup.go
+++ inmemory/cleanup.go
@@ -3,6 +3,7 @@ package inmemory
 import (
 	"runtime"
 	"sort"
+	"time"
 
 	"github.com/Sirupsen/logrus"
 )
@@ -22,7 +23,7 @@ const (
 type CleanupFunc func(*InMemoryCache)
 
 type lastAccessKeyPair struct {
-	LastAccess int64
+	lastAccess int64
 	key        string
 }
 
@@ -30,38 +31,48 @@ type lruPairs []*lastAccessKeyPair
 
 func (a lruPairs) Len() int           { return len(a) }
 func (a lruPairs) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
-func (a lruPairs) Less(i, j int) bool { return a[i].LastAccess < a[j].LastAccess }
+func (a lruPairs) Less(i, j int) bool { return a[i].lastAccess < a[j].lastAccess }
 
-func lruCleanup(c *InMemoryCache) {
-	n := len(c.items)
-	if float64(n)*c.config.CleanupPercent < 1 {
-		return
-	}
-	pairs := make(lruPairs, 0, n)
-	for key, i := range c.items {
-		pairs = append(pairs, &lastAccessKeyPair{i.LastAccess.Unix(), key})
-	}
+func lruItems(pairs lruPairs, percent float64) lruPairs {
 	sort.Sort(pairs)
-	n = len(pairs)
-	k := (int(float64(n) * c.config.CleanupPercent)) - 1
-	if k < 0 {
-		k = 0
-	}
+	n := len(pairs)
+	k := (int(float64(n) * percent * 0.01))
 	if k >= n {
 		k = n - 1
 	}
-	logrus.Debugf("[InMemoryCache:lruCleanup] Rmoving %d items (last pair=%v)", k, pairs[k])
-	c.mutex.Lock()
-	for i := 0; i <= k; i++ {
-		delete(c.items, pairs[i].key)
-	}
-	c.mutex.Unlock()
+	return pairs[0:k]
 }
 
 func heapBasedCleanup(cache *InMemoryCache) {
 	var mem runtime.MemStats
 	runtime.ReadMemStats(&mem)
 	if mem.HeapAlloc > cache.config.CleanupHeapTarget {
-		lruCleanup(cache)
+		logrus.Debugf("[InMemoryCache:lruCleanup] HeapAlloc=%d - Cleanup is triggered", mem.HeapAlloc)
+
+		cache.mutex.RLock()
+		n := len(cache.items)
+		pairs := make(lruPairs, n)
+		j := 0
+		for key, i := range cache.items {
+			pairs[j] = &lastAccessKeyPair{i.LastAccess.Unix(), key}
+			j++
+		}
+		cache.mutex.RUnlock()
+
+		lruItems(pairs, cache.config.CleanupPercent)
+		k := len(pairs)
+		seconds := time.Now().Unix() - pairs[k-1].lastAccess
+		logrus.Debugf("[InMemoryCache:lruCleanup] Rmoving %d items (most recent was accessed %d seconds ago)", k, seconds)
+
+		cache.mutex.Lock()
+		for _, item := range pairs {
+			delete(cache.items, item.key)
+		}
+		cache.mutex.Unlock()
+
+		logrus.Debugf("[InMemoryCache:lruCleanup] Calling GC")
+		runtime.GC()
+		runtime.ReadMemStats(&mem)
+		logrus.Debugf("[InMemoryCache:lruCleanup] New HeapAlloc=%d", mem.HeapAlloc)
 	}
 }
---
+++ inmemory/cleanup_test.go
@@ -0,0 +1,29 @@
+package inmemory
+
+import "testing"
+
+func TestLRUItems(t *testing.T) {
+	pairs := make(lruPairs, 10)
+	pairs[0] = &lastAccessKeyPair{1070, "1070"}
+	pairs[1] = &lastAccessKeyPair{1050, "1050"}
+	pairs[2] = &lastAccessKeyPair{1030, "1030"}
+	pairs[3] = &lastAccessKeyPair{1060, "1060"}
+	pairs[4] = &lastAccessKeyPair{1040, "1040"}
+	pairs[5] = &lastAccessKeyPair{1080, "1080"}
+	pairs[6] = &lastAccessKeyPair{1090, "1090"}
+	pairs[7] = &lastAccessKeyPair{1020, "1020"}
+	pairs[8] = &lastAccessKeyPair{1000, "1000"}
+	pairs[9] = &lastAccessKeyPair{1010, "1010"}
+
+	newPairs := lruItems(pairs, 20.0)
+
+	if len(newPairs) != 2 {
+		t.Fatal("unexpected len(newPairs):", len(newPairs))
+	}
+	if newPairs[0].lastAccess != 1000 {
+		t.Fatal("unexpected newPairs[0]:", newPairs[0].key)
+	}
+	if newPairs[1].lastAccess != 1010 {
+		t.Fatal("unexpected newPairs[1]:", newPairs[1].key)
+	}
+}
